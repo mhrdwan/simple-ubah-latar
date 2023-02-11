a282fe47d3eb1673229ef8d7f08336243aa9c8b2
mhrdwan <futuremhr@gmail.com>
2023-02-11 16:52:33 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let inputHexa = document.getElementById("inputHexa");
+let targetObject = document.getElementById("targetObject");
+
+function ubahLatar() {
+  targetObject.style.backgroundColor = inputHexa.value;
+}
